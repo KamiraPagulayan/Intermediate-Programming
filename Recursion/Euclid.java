b9ce7ac880e2b47d2e02efcfcdaed8a278f1bd2e
Kalli <111488406+KamiraPagulayan@users.noreply.github.com>
2023-03-12 18:26:53 +0800

    Add files via upload

---
+++ Recursion/Euclid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Euclid {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the First Number to find GCD: ");
+        int num1=sc.nextInt();
+        System.out.print("Enter the Second Number to find GCD: ");
+        int num2=sc.nextInt();
+        
+        System.out.print("The GCD of "+num1+" and "+num2+" is: "+euclids(num1, num2));
+    }
+    public static int euclids(int num1,int num2) {
+
+        if (num2==0) {
+            return num1;
+        }
+        return euclids(num2, num1%num2);
+    }
+}
